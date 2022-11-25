f988909ee76d59dee164205845a4b482ac8a36ba
hyooa <hyoyung3231@gmail.com>
2022-11-25 17:31:05 +0900

    완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import TodoApp from './containers/TodoApp';
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from './containers/Main';
 
 function App() {
   return (
     <div className="App">
-      {/* <TodoApp></TodoApp> */}
-      {/* <Main></Main> */}
       <Routes>
         <Route path='/todoApp' element={<TodoApp />}></Route>
         <Route path='/' element={<Main />}></Route>
